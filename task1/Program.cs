636f5aaa55bee9af7b573d7121452254b926cac1
Natalya <alkinatalya@yandex.com>
2022-10-19 20:30:30 +0400

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int a = Prompt("Введите трехзначное число > ");
+if (a < 1000 && a > 99)
+{
+    int b = a / 10;
+    int result = b % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
