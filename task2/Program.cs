f339795958c2a1fe9c20be976f84e504cc7480cc
Natalya <alkinatalya@yandex.com>
2022-10-19 20:31:12 +0400

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int firstNumber = Prompt("Введите любое число > ");
+if (firstNumber >= 0)
+{
+    if (firstNumber > 99)
+    {
+        while (firstNumber >= 999)
+        {
+            firstNumber = firstNumber / 10;
+        }
+        int result = firstNumber % 10;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+else
+{
+    if (firstNumber < -99)
+    {
+        while (firstNumber <= -999)
+        {
+            firstNumber = firstNumber / 10;
+        }
+        int result = firstNumber % 10;
+        result = result * -1;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
